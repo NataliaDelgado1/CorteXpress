c059affda59d2afd5a268ebaf1766acde85213b1
SantiagoUrbano <jeiversantiago@udenar.edu.co>
2025-03-30 16:57:04 -0500

    Actualización de la base de datos

--- docker/backup.sql
+++ docker/backup.sql
@@ -28,7 +28,7 @@ CREATE TABLE `barberos` (
   `nombre` varchar(100) NOT NULL,
   `especialidad` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,9 @@ CREATE TABLE `barberos` (
 
 LOCK TABLES `barberos` WRITE;
 /*!40000 ALTER TABLE `barberos` DISABLE KEYS */;
+INSERT INTO `barberos` VALUES
+(1,'Oscar Vasquez','Cortes modernos'),
+(2,'Juan López','Barba y afeitado');
 /*!40000 ALTER TABLE `barberos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,6 +56,7 @@ CREATE TABLE `citas` (
   `barbero_id` int(11) DEFAULT NULL,
   `servicio_id` int(11) DEFAULT NULL,
   `fecha` datetime NOT NULL,
+  `hora` time NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cliente_id` (`cliente_id`),
   KEY `barbero_id` (`barbero_id`),
@@ -60,7 +64,7 @@ CREATE TABLE `citas` (
   CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
   CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`barbero_id`) REFERENCES `barberos` (`id`),
   CONSTRAINT `citas_ibfk_3` FOREIGN KEY (`servicio_id`) REFERENCES `servicios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,6 +73,11 @@ CREATE TABLE `citas` (
 
 LOCK TABLES `citas` WRITE;
 /*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+INSERT INTO `citas` VALUES
+(1,2,1,1,'2025-04-06 00:00:00','08:30:00'),
+(2,2,1,1,'2025-04-06 00:00:00','16:30:00'),
+(3,2,1,1,'2025-04-06 00:00:00','19:30:00'),
+(4,2,1,1,'2025-04-06 00:00:00','25:30:00');
 /*!40000 ALTER TABLE `citas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +93,7 @@ CREATE TABLE `clientes` (
   `nombre` varchar(100) NOT NULL,
   `telefono` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,6 +102,12 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES
+(1,'santiago','3138789865'),
+(2,'santiago','3138789865'),
+(3,'santiago','3138789865'),
+(4,'santiago','3138789865'),
+(5,'santiago','3136726789');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,13 +121,13 @@ DROP TABLE IF EXISTS `horarios`;
 CREATE TABLE `horarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `barbero_id` int(11) DEFAULT NULL,
-  `dia` varchar(20) NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_fin` time NOT NULL,
+  `fecha` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `barbero_id` (`barbero_id`),
   CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`barbero_id`) REFERENCES `barberos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,6 +136,10 @@ CREATE TABLE `horarios` (
 
 LOCK TABLES `horarios` WRITE;
 /*!40000 ALTER TABLE `horarios` DISABLE KEYS */;
+INSERT INTO `horarios` VALUES
+(7,1,'07:30:00','15:30:00','2025-04-06'),
+(8,2,'09:00:00','17:00:00','2025-04-07'),
+(9,1,'10:30:00','19:30:00','2025-04-08');
 /*!40000 ALTER TABLE `horarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,13 +152,13 @@ DROP TABLE IF EXISTS `pagos`;
 /*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pagos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cita_id` int(11) DEFAULT NULL,
+  `citaId` int(11) DEFAULT NULL,
   `monto` decimal(10,2) NOT NULL,
-  `metodo_pago` varchar(50) NOT NULL,
+  `metodo` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `cita_id` (`cita_id`),
-  CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`cita_id`) REFERENCES `citas` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+  KEY `cita_id` (`citaId`),
+  CONSTRAINT `pagos_ibfk_1` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +167,8 @@ CREATE TABLE `pagos` (
 
 LOCK TABLES `pagos` WRITE;
 /*!40000 ALTER TABLE `pagos` DISABLE KEYS */;
+INSERT INTO `pagos` VALUES
+(1,1,4000.00,'tarjeta');
 /*!40000 ALTER TABLE `pagos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -163,7 +184,7 @@ CREATE TABLE `servicios` (
   `nombre` varchar(100) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,6 +193,12 @@ CREATE TABLE `servicios` (
 
 LOCK TABLES `servicios` WRITE;
 /*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES
+(1,'Corte de cabello',40000.00),
+(2,'Afeitado',25000.00),
+(3,'Corte + Barba',60000.00),
+(4,'Tinte',70000.00),
+(5,'Masaje capilar',30000.00);
 /*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -184,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-03-30 16:43:35
+-- Dump completed on 2025-03-30 21:55:38
