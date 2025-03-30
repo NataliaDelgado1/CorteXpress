3d69687d80301bdd9ae5b5adf94b3e977a9563cc
SantiagoUrbano <jeiversantiago@udenar.edu.co>
2025-03-30 13:05:10 -0500

    agregue la configuracion para el POST de los archivos models.js y routes.js

--- backend/models/cliente.js
+++ backend/models/cliente.js
@@ -4,6 +4,12 @@ class Cliente {
     static getAll(callback) {
         db.query("SELECT * FROM clientes", callback);
     }
+
+    
+    static create(data, callback) {
+        const query = "INSERT INTO clientes (nombre, telefono) VALUES (?, ?)";
+        db.query(query, [data.nombre, data.telefono], callback);
+    }
 }
 
 module.exports = Cliente;
