6bacab0e7e58165ffc60a0efee040551dec19db9
Edith Uyaque <emuyaque21B@udenar.edu.co>
2025-04-04 23:39:09 -0500

    Configuracion archivo models tabla cita

--- backend/models/cita.js
+++ backend/models/cita.js
@@ -5,13 +5,22 @@ class Cita {
         db.query("SELECT * FROM citas", callback);
     }
 
-    static create(cita, callback) {
-        const { cliente_id, barbero_id, fecha, hora, servicio_id } = cita;
-        db.query(
-            "INSERT INTO citas (cliente_id, barbero_id, fecha, hora, servicio_id) VALUES (?, ?, ?, ?, ?)",
-            [cliente_id, barbero_id, fecha, hora, servicio_id],
-            callback
-        );
+    static getById(id, callback) {
+        db.query("SELECT * FROM citas WHERE id = ?", [id], callback);
+    }
+
+    static create(data, callback) {
+        const query = "INSERT INTO citas (cliente_id, barbero_id, fecha, hora) VALUES (?, ?, ?, ?)";
+        db.query(query, [data.cliente_id, data.barbero_id, data.fecha, data.hora], callback);
+    }
+
+    static update(id, data, callback) {
+        const query = "UPDATE citas SET cliente_id = ?, barbero_id = ?, fecha = ?, hora = ? WHERE id = ?";
+        db.query(query, [data.cliente_id, data.barbero_id, data.fecha, data.hora, id], callback);
+    }
+
+    static delete(id, callback) {
+        db.query("DELETE FROM citas WHERE id = ?", [id], callback);
     }
 }
 
