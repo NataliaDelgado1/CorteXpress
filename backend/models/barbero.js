537e7cc20baaa71f274a29e2832e883854be0ddc
Edith Uyaque <emuyaque21B@udenar.edu.co>
2025-04-04 23:38:20 -0500

    Configuracion archivo models tabla barbero

--- backend/models/barbero.js
+++ backend/models/barbero.js
@@ -4,6 +4,24 @@ class Barbero {
     static getAll(callback) {
         db.query("SELECT * FROM barberos", callback);
     }
+
+    static getById(id, callback) {
+        db.query("SELECT * FROM barberos WHERE id = ?", [id], callback);
+    }
+
+    static create(data, callback) {
+        const query = "INSERT INTO barberos (nombre, especialidad) VALUES (?, ?)";
+        db.query(query, [data.nombre, data.especialidad], callback);
+    }
+
+    static update(id, data, callback) {
+        const query = "UPDATE barberos SET nombre = ?, especialidad = ? WHERE id = ?";
+        db.query(query, [data.nombre, data.especialidad, id], callback);
+    }
+
+    static delete(id, callback) {
+        db.query("DELETE FROM barberos WHERE id = ?", [id], callback);
+    }
 }
 
 module.exports = Barbero;
