182a1213601d3dd70d1cb25f1f665b9d20d4ef58
Edith Uyaque <emuyaque21B@udenar.edu.co>
2025-04-04 23:42:42 -0500

    Configuracion archivo models tabla servicio

--- backend/models/servicio.js
+++ backend/models/servicio.js
@@ -4,6 +4,24 @@ class Servicio {
     static getAll(callback) {
         db.query("SELECT * FROM servicios", callback);
     }
+
+    static getById(id, callback) {
+        db.query("SELECT * FROM servicios WHERE id = ?", [id], callback);
+    }
+
+    static create(data, callback) {
+        const query = "INSERT INTO servicios (nombre, precio) VALUES (?, ?)";
+        db.query(query, [data.nombre, data.precio], callback);
+    }
+
+    static update(id, data, callback) {
+        const query = "UPDATE servicios SET nombre = ?, precio = ? WHERE id = ?";
+        db.query(query, [data.nombre, data.precio, id], callback);
+    }
+
+    static delete(id, callback) {
+        db.query("DELETE FROM servicios WHERE id = ?", [id], callback);
+    }
 }
 
 module.exports = Servicio;
