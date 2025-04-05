3e96d34177393dc842d2dffcf83eb03856fbe405
Edith Uyaque <emuyaque21B@udenar.edu.co>
2025-04-04 23:40:19 -0500

    Configuracion archivo models tabla cliente

--- backend/models/cliente.js
+++ backend/models/cliente.js
@@ -1,14 +1,31 @@
 const db = require("../config/db");
 
 class Cliente {
+    // Obtener todos los clientes
     static getAll(callback) {
         db.query("SELECT * FROM clientes", callback);
     }
 
-    
+    // Obtener un cliente por ID
+    static getById(id, callback) {
+        db.query("SELECT * FROM clientes WHERE id = ?", [id], callback);
+    }
+
+    // Crear un nuevo cliente
     static create(data, callback) {
         const query = "INSERT INTO clientes (nombre, telefono) VALUES (?, ?)";
         db.query(query, [data.nombre, data.telefono], callback);
+    }
+
+    // Actualizar un cliente existente
+    static update(id, data, callback) {
+        const query = "UPDATE clientes SET nombre = ?, telefono = ? WHERE id = ?";
+        db.query(query, [data.nombre, data.telefono, id], callback);
+    }
+
+    // Eliminar un cliente
+    static delete(id, callback) {
+        db.query("DELETE FROM clientes WHERE id = ?", [id], callback);
     }
 }
 
