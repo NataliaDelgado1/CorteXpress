1683a2e5ae9952f2b7dacc20854147e0d3f219c5
SantiagoUrbano <jeiversantiago@udenar.edu.co>
2025-04-06 18:17:37 -0500

    modifiqué la tabla barberos del archivo controllers

--- backend/controllers/barberosController.js
+++ backend/controllers/barberosController.js
@@ -1,3 +1,5 @@
+const db = require("../config/db");
+
 exports.getBarberos = (req, res) => {
     db.query("SELECT * FROM barberos", (err, result) => {
         if (err) res.status(500).send("Error al obtener barberos");
@@ -10,5 +12,22 @@ exports.createBarbero = (req, res) => {
     db.query("INSERT INTO barberos (nombre, experiencia) VALUES (?, ?)", [nombre, experiencia], (err) => {
         if (err) res.status(500).send("Error al crear barbero");
         else res.status(201).send("Barbero creado exitosamente");
+    });
+};
+
+exports.updateBarbero = (req, res) => {
+    const { id } = req.params;
+    const { nombre, experiencia } = req.body;
+    db.query("UPDATE barberos SET nombre = ?, experiencia = ? WHERE id = ?", [nombre, experiencia, id], (err) => {
+        if (err) res.status(500).send("Error al actualizar barbero");
+        else res.send("Barbero actualizado exitosamente");
+    });
+};
+
+exports.deleteBarbero = (req, res) => {
+    const { id } = req.params;
+    db.query("DELETE FROM barberos WHERE id = ?", [id], (err) => {
+        if (err) res.status(500).send("Error al eliminar barbero");
+        else res.send("Barbero eliminado exitosamente");
     });
 };
