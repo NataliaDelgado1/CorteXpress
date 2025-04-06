936cb6b58a3ede5d2b349a9ebcf88edcd99cad52
SantiagoUrbano <jeiversantiago@udenar.edu.co>
2025-04-06 18:19:45 -0500

    modifiqué la tabla horarios del archivo controllers

--- backend/controllers/horariosController.js
+++ backend/controllers/horariosController.js
@@ -1,3 +1,5 @@
+const db = require("../config/db");
+
 exports.getHorarios = (req, res) => {
     db.query("SELECT * FROM horarios", (err, result) => {
         if (err) res.status(500).send("Error al obtener horarios");
@@ -6,9 +8,30 @@ exports.getHorarios = (req, res) => {
 };
 
 exports.createHorario = (req, res) => {
-    const { barberoId, dia, horaInicio, horaFin } = req.body;
-    db.query("INSERT INTO horarios (barberoId, dia, horaInicio, horaFin) VALUES (?, ?, ?, ?)", [barberoId, dia, horaInicio, horaFin], (err) => {
-        if (err) res.status(500).send("Error al crear horario");
-        else res.status(201).send("Horario creado exitosamente");
+    const { dia, hora_inicio, hora_fin } = req.body;
+    db.query("INSERT INTO horarios (dia, hora_inicio, hora_fin) VALUES (?, ?, ?)",
+        [dia, hora_inicio, hora_fin],
+        (err) => {
+            if (err) res.status(500).send("Error al crear horario");
+            else res.status(201).send("Horario creado exitosamente");
+        });
+};
+
+exports.updateHorario = (req, res) => {
+    const { id } = req.params;
+    const { dia, hora_inicio, hora_fin } = req.body;
+    db.query("UPDATE horarios SET dia = ?, hora_inicio = ?, hora_fin = ? WHERE id = ?",
+        [dia, hora_inicio, hora_fin, id],
+        (err) => {
+            if (err) res.status(500).send("Error al actualizar horario");
+            else res.send("Horario actualizado exitosamente");
+        });
+};
+
+exports.deleteHorario = (req, res) => {
+    const { id } = req.params;
+    db.query("DELETE FROM horarios WHERE id = ?", [id], (err) => {
+        if (err) res.status(500).send("Error al eliminar horario");
+        else res.send("Horario eliminado exitosamente");
     });
 };
